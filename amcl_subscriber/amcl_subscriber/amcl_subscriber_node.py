6589b0f13f4ec1ed2a3d0cde466ac00d07109aa7
demuko <demura@neptune.kanazawa-it.ac.jp>
2024-09-16 12:19:38 +0900

    amcl_poseとodomトピックはコールバックの周期が違い比較しづらいのでタイマーにより同じ周期でログに出力するようにした。

--- amcl_subscriber/amcl_subscriber/amcl_subscriber_node.py
+++ amcl_subscriber/amcl_subscriber/amcl_subscriber_node.py
@@ -1,10 +1,10 @@
 import rclpy
-import tf_transformations
-from rclpy.node import Node                                                                             
+from rclpy.node import Node 
+import tf_transformations as tf_trans                                                                            
 from std_msgs.msg import String                                                                         
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Quaternion
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped, Pose
 from nav_msgs.msg import Odometry
 from tf_transformations import euler_from_quaternion
 
@@ -14,29 +14,29 @@ class AMCLSubscriber(Node):
         self.create_subscription(PoseWithCovarianceStamped,
                                                  'amcl_pose', self.amcl_cb, 10)                  
         self.create_subscription(Odometry,'odom',self.odom_cb, 10)                  
-                                                                                                        
+        self.create_timer(0.1, self.timer_cb)  
+        self.last_amcl_msg = Pose()
+        self.last_odom_msg = Pose()                                                                                      
 
-    def get_pose(self, msg):      # 姿勢の取得
-        x = msg.pose.pose.position.x
-        y = msg.pose.pose.position.y
-        q_x = msg.pose.pose.orientation.x
-        q_y = msg.pose.pose.orientation.y
-        q_z = msg.pose.pose.orientation.z
-        q_w = msg.pose.pose.orientation.w
-        (roll, pitch, yaw) = tf_transformations.euler_from_quaternion((q_x, q_y, q_z, q_w))
-        return x, y, yaw
+    def get_pose(self,msg):      # 姿勢の取得
+        pos = msg.position
+        q = msg.orientation
+        (roll, pitch, yaw) = tf_trans.euler_from_quaternion((q.x, q.y, q.z, q.w))
+        return pos.x, pos.y, yaw
 
-
-    def amcl_cb(self, msg):         # AMCLのコールバック関数
-        x, y, yaw = self.get_pose(msg)
-        self.get_logger().info("AMCL：x={:.2f} y={:.2f}[m] theta={:.2f}[rad/s]"
-             .format(x, y, yaw))
-
-    def odom_cb(self, msg):         # ODOMのコールバック関数
-        x, y, yaw = self.get_pose(msg)
-        self.get_logger().info("ODOM：x={:.2f} y={:.2f}[m] theta={:.2f}[rad/s]"
-             .format(x, y, yaw))
-
+    def amcl_cb(self, msg):         # AMCLのコールバック
+        self.last_amcl_msg = msg.pose.pose
+  
+    def odom_cb(self, msg):         # ODOMのコールバック
+        self.last_odom_msg = msg.pose.pose
+ 
+    def timer_cb(self):             # タイマーコールバック
+        for name, msg in [('AMCL',self.last_amcl_msg),('ODOM',self.last_odom_msg)]:
+            x, y, yaw = self.get_pose(msg)        
+            if msg:
+                self.get_logger().info(f'{name}: x={x:.2f} y={y:.2f} [m] theta={yaw:.2f}[rad/s]')
+            else:
+                self.get_logger().info(f'No {name} received yet.')
         
 def main(args=None):                                                                                    
     rclpy.init(args=args)                                                                               
